ae5372917afaeb5aa9374ffbb429bf486c0e9535
abinaie <amir.b.kondolojy@gamil.com>
2021-01-09 13:58:31 -0500

    init

---
+++ src/robots.js
@@ -0,0 +1,27 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Amir',
+        username: 'BigAmir',
+        email: 'amir@gmil.com'
+    },
+    {
+        id:2,
+        name: 'Halat',
+        username: 'Mahrooi',
+        email: 'halat@gmil.com'
+    },    
+    {
+        id:3,
+        name: 'Ehsan',
+        username: 'Esi',
+        email: 'ehsan@gmil.com'
+    },
+    {
+        id:4,
+        name: 'Shahin',
+        username: 'Maman',
+        email: 'eh@gmil.com'
+    },
+
+]
