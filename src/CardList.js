79fe66c405cff37b82caf0eb82b50ca626ff027c
abinaie <amir.b.kondolojy@gamil.com>
2021-01-09 15:17:06 -0500

    new updates

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i) => {
+    const cardArray = robots.map((user, i) => {
         return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     })
     return (
         <React.StrictMode>
-            {cardComponent}
+            {cardArray}
       </React.StrictMode>        
     );
 }
