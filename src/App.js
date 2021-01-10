a870de2523d258cded8b4c550b35330e37be10e4
abinaie <amir.b.kondolojy@gamil.com>
2021-01-10 17:32:36 -0500

    scrollable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css'
 
 class App extends Component{
@@ -31,11 +32,13 @@ class App extends Component{
             return <h1>Loading</h1>
         }else{
             return (
-                <div className = 'tc'>
+               <div className = 'tc'>
                 <h1 className='f1'> RobotFriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots = {filteredrobots}/>
-                </div>
+                <Scroll>
+                    <CardList robots = {filteredrobots}/>
+                </Scroll>    
+               </div>
             );
         }
     }
